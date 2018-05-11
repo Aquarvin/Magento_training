8030ef201523f48c4a7a5ee4f868209a56534ea9
Aleksandr Ishchenko <fevius@gmail.com>
2018-05-11 18:32:12 +0300

    Unit 2, Module 3, 2.5.4. Make the “Hello World” controller you just created redirect to a specific category page.

--- app/code/Training/Unit2/Controller/Action/Index.php
+++ app/code/Training/Unit2/Controller/Action/Index.php
@@ -12,5 +12,6 @@ class Index extends \Magento\Framework\App\Action\Action
     public function execute()
     {
         $this->getResponse()->appendBody("HELLO WORLD");
+        $this->_redirect('catalog/category/view/id/289');
     }
 }
