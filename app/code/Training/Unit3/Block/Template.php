59a0ee941295fdba0efed90352c3d4c2f1030b20
Aleksandr Ishchenko <fevius@gmail.com>
2018-05-25 22:42:38 +0300

    Unit 3, module 3.6.2 Empty Block

--- app/code/Training/Unit3/Block/Template.php
+++ app/code/Training/Unit3/Block/Template.php
@@ -4,8 +4,4 @@ namespace Training\Unit3\Block;
 
 class Template extends \Magento\Framework\View\Element\Template
 {
-    protected function _toHtml()
-    {
-        return "<b>Hello world from block!</b>";
-    }
 }
