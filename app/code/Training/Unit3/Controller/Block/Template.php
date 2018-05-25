36bb0c8c021ba9869063bbd8ce1a72683aca3072
Aleksandr Ishchenko <fevius@gmail.com>
2018-05-25 22:28:59 +0300

    Unit 3, module 3.6.2 Create a template block and a custom template file for it. Render the block in the controller.

---
+++ app/code/Training/Unit3/Controller/Block/Template.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Product controller.
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Training\Unit3\Controller\Block;
+
+class Template extends \Magento\Framework\App\Action\Action
+{
+    public function execute()
+    {
+        $block = $this->_view->getLayout()->createBlock('Training\Unit3\Block\Template');
+        $block->setTemplate('template.html');
+        $this->getResponse()->appendBody($block->toHtml());
+    }
+}
